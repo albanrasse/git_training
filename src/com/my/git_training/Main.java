7827f216403ea106a8b967191a6f02258233cf60
Alban Rasse <alban.rasse@gmail.com>
2022-01-02 11:24:49 +0100

    premier commit

---
+++ src/com/my/git_training/Main.java
@@ -0,0 +1,21 @@
+package com.my.git_training;
+
+public class Main {
+
+	/*
+	 * Projet lié au repository github
+	 * https://github.com/albanrasse/git_training.git
+	 * 
+	 */
+	
+	
+	//*********************************************************************************
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("HELLO WORD 2");
+
+	}
+
+}
